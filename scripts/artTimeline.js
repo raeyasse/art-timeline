67e538230fbffab9df540f2860fce2b0eecfa0f4
raeyasse <raeytesfay@gmail.com>
2026-05-13 20:45:50 -0400

    fixed CORS error by changing fetch formatting and cross referencing API Documentation for best practice

--- scripts/artTimeline.js
+++ scripts/artTimeline.js
@@ -1,9 +1,9 @@
 let centuries = [
-    { label: "15th Century", url: "https://api.artic.edu/api/v1/artworks/search?query[range][date_start][gte]=1400&query[range][date_start][lte]=1499&fields=id,title,date_start,artist_title,image_id&limit=20" },
-    { label: "17th Century", url: "https://api.artic.edu/api/v1/artworks/search?query[range][date_start][gte]=1600&query[range][date_start][lte]=1699&fields=id,title,date_start,artist_title,image_id&limit=20" },
-    { label: "18th Century", url: "https://api.artic.edu/api/v1/artworks/search?query[range][date_start][gte]=1700&query[range][date_start][lte]=1799&fields=id,title,date_start,artist_title,image_id&limit=20" },
-    { label: "19th Century", url: "https://api.artic.edu/api/v1/artworks/search?query[range][date_start][gte]=1800&query[range][date_start][lte]=1899&fields=id,title,date_start,artist_title,image_id&limit=20" },
-    { label: "20th Century", url: "https://api.artic.edu/api/v1/artworks/search?query[range][date_start][gte]=1900&query[range][date_start][lte]=1999&fields=id,title,date_start,artist_title,image_id&limit=20" }
+    { label: "15th Century", url: "https://api.artic.edu/api/v1/artworks/search?params=%7B%22query%22%3A%7B%22range%22%3A%7B%22date_start%22%3A%7B%22gte%22%3A1400%2C%22lte%22%3A1499%7D%7D%7D%7D&fields=id,title,date_start,artist_title,image_id&limit=20" },
+    { label: "17th Century", url: "https://api.artic.edu/api/v1/artworks/search?params=%7B%22query%22%3A%7B%22range%22%3A%7B%22date_start%22%3A%7B%22gte%22%3A1600%2C%22lte%22%3A1699%7D%7D%7D%7D&fields=id,title,date_start,artist_title,image_id&limit=20" },
+    { label: "18th Century", url: "https://api.artic.edu/api/v1/artworks/search?params=%7B%22query%22%3A%7B%22range%22%3A%7B%22date_start%22%3A%7B%22gte%22%3A1700%2C%22lte%22%3A1799%7D%7D%7D%7D&fields=id,title,date_start,artist_title,image_id&limit=20" },
+    { label: "19th Century", url: "https://api.artic.edu/api/v1/artworks/search?params=%7B%22query%22%3A%7B%22range%22%3A%7B%22date_start%22%3A%7B%22gte%22%3A1800%2C%22lte%22%3A1899%7D%7D%7D%7D&fields=id,title,date_start,artist_title,image_id&limit=20" },
+    { label: "20th Century", url: "https://api.artic.edu/api/v1/artworks/search?params=%7B%22query%22%3A%7B%22range%22%3A%7B%22date_start%22%3A%7B%22gte%22%3A1900%2C%22lte%22%3A1999%7D%7D%7D%7D&fields=id,title,date_start,artist_title,image_id&limit=20" }
 ]
 
 let container = document.getElementById("artwork-container")
